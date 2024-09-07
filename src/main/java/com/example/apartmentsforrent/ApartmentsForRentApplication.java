4c6121a50ff008da5c4efa46cc4784b5b34d57d9
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2024-09-07 21:25:56 +0300

    Create project

---
+++ src/main/java/com/example/apartmentsforrent/ApartmentsForRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.apartmentsforrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentsForRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartmentsForRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apartmentsforrent/ApartmentsForRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartmentsforrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentsForRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

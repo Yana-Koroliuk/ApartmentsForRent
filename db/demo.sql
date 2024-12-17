07e9e88eed9765d79ca4df9283652ba5d3833156
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2024-12-17 04:10:06 +0200

    Update demo.sql and application.properties

--- db/demo.sql
+++ db/demo.sql
@@ -20,41 +20,41 @@ VALUES ('Василь',
         'password3');
 
 INSERT INTO details (address, area, year, price, floor, quantity_of_rooms)
-VALUES ('Vinnytsia, Kosmonavtiv Ave, 43, apt. 83', 100, '1965-01-01', 4000, 3, 2);
+VALUES ('Vinnytsia, Kosmonavtiv Ave, 43, apt. 83', 100, '1965', 4000, 3, 2);
 
 INSERT INTO details (address, area, year, price, floor, quantity_of_rooms)
-VALUES ('Kyiv, Peremohy Street, 5, apt. 46', 100, '1995-01-01', 4000, 3, 2);
+VALUES ('Kyiv, Peremohy Street, 5, apt. 46', 100, '1995', 4000, 3, 2);
 
 INSERT INTO details (address, area, year, price, floor, quantity_of_rooms)
-VALUES ('Zhytomyr, Chornoi Ave, 12, apt. 90', 150.23, '1958-01-01', 1000, 5, 1);
+VALUES ('Zhytomyr, Chornoi Ave, 12, apt. 90', 150.23, '1958', 1000, 5, 1);
 
 INSERT INTO details (address, area, year, price, floor, quantity_of_rooms)
-VALUES ('м. Київ, вул. Політехнічна, 41, кв. 316',  210.50, '1945-01-01', 7000, 4, 1);
+VALUES ('м. Київ, вул. Політехнічна, 41, кв. 316',  210.50, '1945', 7000, 4, 1);
 
 INSERT INTO details (address, area, year, price, floor, quantity_of_rooms)
-VALUES ('м. Київ, вул. Банкова, 11, кв. 1', 300, '1936-01-01', 1000000, 6, 10);
+VALUES ('м. Київ, вул. Банкова, 11, кв. 1', 300, '1936', 1000000, 6, 10);
 
-INSERT INTO descriptions (condition, type, additional_info)
+INSERT INTO descriptions (condition, building_type, additional_info)
 VALUES ('Дуже хороші умови',
         'BRICK',
         NULL);
 
-INSERT INTO descriptions (condition, type, additional_info)
+INSERT INTO descriptions (condition, building_type, additional_info)
 VALUES ('Скоро розвалиться',
         'FOAM_BLOCK',
         'Після того, як ми привернули увагу, погляньте на шедевральні умови в деталях до кімнати');
 
-INSERT INTO descriptions (condition, type, additional_info)
+INSERT INTO descriptions (condition, building_type, additional_info)
 VALUES ('Ремонт найвищого зразка',
         'SILICATE_BRICK',
         NULL);
 
-INSERT INTO descriptions (condition, type, additional_info)
+INSERT INTO descriptions (condition, building_type, additional_info)
 VALUES ('Чорновий ремонт',
         'MONOLITH',
         'Проведено чорновий ремонт, подальше можливо зробити за своїм смаком');
 
-INSERT INTO descriptions (condition, type, additional_info)
+INSERT INTO descriptions (condition, building_type, additional_info)
 VALUES ('Євроремонт',
         'PANEL',
         'Квартира розроблена та побудована згідно всіх європейських стандартів');
